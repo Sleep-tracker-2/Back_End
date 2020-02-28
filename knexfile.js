4ad756e9cc97add8b1a255c700c6119e0fd12c15
Cody-Hayes97 <hayesmichael2@gmail.com>
2020-02-28 15:37:03 -0800

    fixed

--- knexfile.js
+++ knexfile.js
@@ -20,6 +20,25 @@ module.exports = {
       }
     }
   },
+  production: {
+    client: "sqlite3",
+    useNullAsDefault: true,
+    dateStrings: true,
+    connection: {
+      filename: "./data/sleep.db3"
+    },
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done);
+      }
+    }
+  },
   testing: {
     client: "sqlite3",
     connection: {
