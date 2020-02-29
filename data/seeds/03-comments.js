cfe9535b555f6aedb11ccffd893ffee952f33b4a
Cody-Hayes97 <hayesmichael2@gmail.com>
2020-02-28 16:30:20 -0800

    fixed seed

--- data/seeds/03-comments.js
+++ data/seeds/03-comments.js
@@ -5,9 +5,9 @@ exports.seed = function(knex) {
     .then(function() {
       // Inserts seed entries
       return knex("comments").insert([
-        { id: 1, comment: "my sleep last night was amazing!", sleep_id: 5 },
-        { id: 2, comment: "my sleep was terrible!", sleep_id: 5 },
-        { id: 3, comment: "end my suffering!", sleep_id: 7 }
+        { id: 1, comment: "my sleep last night was amazing!", sleep_id: 3 },
+        { id: 2, comment: "my sleep was terrible!", sleep_id: 3 },
+        { id: 3, comment: "end my suffering!", sleep_id: 1 }
       ]);
     });
 };
