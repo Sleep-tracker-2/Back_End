6f500b1df0bc133438b3ef216bc1b00b743c4787
Cody-Hayes97 <hayesmichael2@gmail.com>
2020-03-01 18:05:53 -0800

    fixed id

--- data/seeds/02-sleep.js
+++ data/seeds/02-sleep.js
@@ -6,7 +6,7 @@ exports.seed = function(knex) {
       // Inserts seed entries
       return knex("sleep").insert([
         {
-          id: 1,
+          sleep_id: 1,
           started_sleep: "07:54",
           ended_sleep: "12:30",
           user_id: 1,
@@ -14,7 +14,7 @@ exports.seed = function(knex) {
           comment: "my sleep was amazing!"
         },
         {
-          id: 2,
+          sleep_id: 2,
           started_sleep: "07:54",
           ended_sleep: "10:25",
           user_id: 2,
@@ -22,7 +22,7 @@ exports.seed = function(knex) {
           comment: "my sleep was terrible!"
         },
         {
-          id: 3,
+          sleep_id: 3,
           started_sleep: "12:54",
           ended_sleep: "02:30",
           user_id: 1,
